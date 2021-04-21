fc24a24cb2ffab5b579941e8faf4dda612ee587a
瑩 佳 <a11910691110@gmail.com>
2021-04-22 05:44:31 +0800

    痾...不知道是否對的作業

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -7,8 +7,27 @@ using System.Web.UI.WebControls;
 
 namespace _4HW {
     public partial class DropdownListShow : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ddl_Area.Items.Count == 0)
+            {
+                ddl_Area.Items.Add("北區");
+                ddl_Area.Items.Add("中區");
+            }
+            if(ddl_Area.SelectedValue== "北區")
+            {
+                ddl_Place.Items.Clear();
+                ddl_Place.Items.Add("基隆");
+                ddl_Place.Items.Add("台北");
+                ddl_Place.Items.Add("新北");
+            }
+            else
+            {
+                ddl_Place.Items.Clear();
+                ddl_Place.Items.Add("苗栗");
+                ddl_Place.Items.Add("台中");
+                ddl_Place.Items.Add("南投");
+            }
         }
     }
 }
